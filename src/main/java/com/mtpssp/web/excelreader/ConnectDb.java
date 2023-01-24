d10c4f86ebb887508e8e17ccb83a879166896802
kprit-dev <pritamvkh007@gmail.com>
2023-01-24 14:55:20 +0530

    SQLException corrected

--- src/main/java/com/mtpssp/web/excelreader/ConnectDb.java
+++ src/main/java/com/mtpssp/web/excelreader/ConnectDb.java
@@ -1,8 +1,10 @@
 package com.mtpssp.web.excelreader;
-import java.sql.*; 
+import java.sql.*;
+import java.util.ArrayList; 
 public class ConnectDb {
 	public static String GetDbData(){ 
 		String name="";
+		ArrayList<String> a=new ArrayList<String>();
 		try{  
 			
 		Class.forName("com.mysql.jdbc.Driver");  
@@ -13,10 +15,10 @@ public class ConnectDb {
 		ResultSet rs=stmt.executeQuery("select * from sabhasad");  
 		while(rs.next())  
 		System.out.println(rs.getString(1)+"  "+rs.getString(2)+"  "+rs.getString(3)); 
-		name=rs.getString(1);
+		a.add(rs.getString(1));
 		con.close();  
 		}catch(Exception e){ System.out.println(e);} 
-		return name;
+		return name=a.get(0);
 		} 
 	
 
